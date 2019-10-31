cb9960450d503e6b5bd1185e64b55d3a020bf3a3
roberto-suner <roberto.suner@entertainment.com.au>
2019-10-31 17:46:59 +1100

    added sort test

--- tests/WooliesX.Technical.Exercises.UnitTests/ControllerTests/ExerciseControllerTests.cs
+++ tests/WooliesX.Technical.Exercises.UnitTests/ControllerTests/ExerciseControllerTests.cs
@@ -1,4 +1,8 @@
-﻿using NSubstitute;
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using WooliesX.Technical.Exercises.Controllers;
 using WooliesX.Technical.Exercises.Models;
 using WooliesX.Technical.Exercises.Services;
@@ -34,6 +38,46 @@ namespace WooliesX.Technical.Exercises.UnitTests.ControllerTests
             Assert.IsAssignableFrom<User>(result.Value);
             Assert.Equal(user.Name, result.Value.Name);
             Assert.Equal(user.Token, result.Value.Token);
+        }
+
+        [Theory()]
+        [InlineData("")]
+        [InlineData("Low")]
+        [InlineData("High")]
+        [InlineData("Ascending")]
+        [InlineData("Descending")]
+        [InlineData("Recommended")]
+        public async Task SortProducts(string sortOption)
+        {
+            // Arrange
+            var products = new List<Product>()
+            {
+                new Product
+                {
+                    Name = "Test Product A",
+                    Price = 99,
+                    Quantity = 1
+                },
+                new Product
+                {
+                    Name = "Test Product B",
+                    Price = 80,
+                    Quantity = 1
+                }
+            };
+            _productService.GetProductsAsync(sortOption).Returns(Task.FromResult(products.AsEnumerable()));
+
+            // Act
+            var actionResult = await _controller.SortProducts(sortOption);
+
+            var result = actionResult.Result as OkObjectResult;
+            var productsResult = (IEnumerable<Product>)result.Value;
+
+            // Assert
+            Assert.IsAssignableFrom<IEnumerable<Product>>(productsResult);
+            Assert.Equal(productsResult.FirstOrDefault().Name, productsResult.FirstOrDefault().Name);
+            Assert.Equal(productsResult.FirstOrDefault().Price, productsResult.FirstOrDefault().Price);
+            Assert.Equal(productsResult.FirstOrDefault().Quantity, productsResult.FirstOrDefault().Quantity);
         }
     }
 }
