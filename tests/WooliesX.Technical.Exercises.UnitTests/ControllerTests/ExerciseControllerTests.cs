686ec9281d3877e40767a3457bf3f9a997d20fed
roberto-suner <roberto.suner@entertainment.com.au>
2019-10-31 13:35:58 +1100

    added GetUser unit tests

---
+++ tests/WooliesX.Technical.Exercises.UnitTests/ControllerTests/ExerciseControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using NSubstitute;
+using WooliesX.Technical.Exercises.Controllers;
+using WooliesX.Technical.Exercises.Models;
+using WooliesX.Technical.Exercises.Services;
+using Xunit;
+
+namespace WooliesX.Technical.Exercises.UnitTests.ControllerTests
+{
+    public class ExerciseControllerTests
+    {
+        private readonly IUserService _userService;
+        public ExerciseControllerTests()
+        {
+            _userService = Substitute.For<IUserService>();
+        }
+
+        [Fact]
+        public void GetReturnsUser()
+        {
+            // Arrange
+            var user = new User { Name = "Test", Token = "abc123" };
+            _userService.GetUser().Returns(user);
+
+            var controller = new ExerciseController(_userService);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.IsAssignableFrom<User>(result.Value);
+            Assert.Equal(user.Name, result.Value.Name);
+            Assert.Equal(user.Token, result.Value.Token);
+        }
+    }
+}
